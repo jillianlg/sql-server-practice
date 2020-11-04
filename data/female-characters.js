82336cf1d3cf8310cd2f086cb82b30a141ba162c
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-11-04 14:24:29 -0800

    update drop-tables, add publisher_id and array to data

--- data/female-characters.js
+++ data/female-characters.js
@@ -3,37 +3,50 @@ module.exports = [
     name: 'Wonder Woman',
     evil_factor: 0,
     feature_film: true,
-    publisher: 'DC Comics',
+    publisher_id: 2,
   },
   {
     name: 'Black Widow',
     evil_factor: 2,
     feature_film: true,
-    publisher: 'Marvel',
+    publisher_id: 1,
   },
   {
     name: 'Captin Marvel',
     evil_factor: 0,
     feature_film: true,
-    publisher: 'Marvel',
+    publisher_id: 1,
   },
   {
     name: 'Poison Ivy',
     evil_factor: 9,
     feature_film: false,
-    publisher: 'DC Comics',
+    publisher_id: 2,
   },
   {
     name: 'Tank Girl',
     evil_factor: 2,
     feature_film: true,
-    publisher: 'Dark Horse',
+    publisher_id: 3,
   },
   {
     name: 'Mystique',
     evil_factor: 6,
     feature_film: false,
+    publisher_id: 1,
+  },
+
+];
+
+export const publishers = [
+  {
     publisher: 'Marvel',
+  },
+  {
+    publisher: 'DC Comics',
+  },
+  {
+    publisher: 'Dark Horse',
   },
 
 ];
