b849879485108bbeac99c3591b225d7704643fd8
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-11-04 15:24:05 -0800

    update GET all w/ publisher table join

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,7 +1,7 @@
 const client = require('../lib/client');
 // import our seed data:
 const females = require('./female-characters.js');
-const { publishers } = require('./publishers.js');
+const publishers = require('./publishers.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -26,16 +26,6 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      females.map(females => {
-        return client.query(`
-                    INSERT INTO females (name, evil_factor, feature_film, publisher_id, owner_id)
-                    VALUES ($1, $2, $3, $4, $5);
-                `,
-        [females.name, females.evil_factor, females.feature_film, females.publisher_id, user.id]);
-      })
-    );
-    
-    await Promise.all(
       publishers.map(item => {
         return client.query(`
                       INSERT INTO publishers (publisher)
@@ -45,6 +35,17 @@ async function run() {
         [item.publisher]);
       })
     );
+
+    await Promise.all(
+      females.map(females => {
+        return client.query(`
+                    INSERT INTO females (name, evil_factor, feature_film, publisher_id, owner_id)
+                    VALUES ($1, $2, $3, $4, $5);
+                `,
+        [females.name, females.evil_factor, females.feature_film, females.publisher_id, user.id]);
+      })
+    );
+    
 
     console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
   }
--- data/publishers.js
+++ data/publishers.js
@@ -1,4 +1,4 @@
-export const publishers = [
+module.exports = [
   {
     publisher: 'Marvel',
   },
